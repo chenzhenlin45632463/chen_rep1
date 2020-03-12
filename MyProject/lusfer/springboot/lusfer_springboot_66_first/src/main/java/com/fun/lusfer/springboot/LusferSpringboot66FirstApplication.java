9edadc32d6bdf00c06c217b9dc005f8d8138df12
陈巍 <18381121942@163.com>
2020-03-12 18:00:15 +0800

    lusfer第66、71课程关于springboot的初级用法及thymeleaf

---
+++ MyProject/lusfer/springboot/lusfer_springboot_66_first/src/main/java/com/fun/lusfer/springboot/LusferSpringboot66FirstApplication.java
@@ -0,0 +1,13 @@
+package com.fun.lusfer.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LusferSpringboot66FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LusferSpringboot66FirstApplication.class, args);
+    }
+
+}
---
+++ MyProject/lusfer/springboot/lusfer_springboot_71_thymeleaf/src/main/java/com/fun/lusfer/springboot/LusferSpringboot71ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.fun.lusfer.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LusferSpringboot71ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LusferSpringboot71ThymeleafApplication.class, args);
+    }
+
+}
