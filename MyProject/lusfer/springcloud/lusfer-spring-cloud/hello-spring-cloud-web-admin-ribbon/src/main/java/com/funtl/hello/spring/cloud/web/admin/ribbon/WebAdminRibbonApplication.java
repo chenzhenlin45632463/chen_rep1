ea5c6e463872894538dd8bd09234ab2a34649826
陈巍 <18381121942@163.com>
2020-03-14 10:32:04 +0800

    lusfer——使用熔断器仪表盘监控

--- MyProject/lusfer/springcloud/lusfer-spring-cloud/hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
+++ MyProject/lusfer/springcloud/lusfer-spring-cloud/hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard//使用熔断器仪表盘监控
 @EnableHystrix//添加ribbon方式下的熔断机制
 @SpringBootApplication
 @EnableDiscoveryClient
